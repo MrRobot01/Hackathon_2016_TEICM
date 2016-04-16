ace943028dabdf306bf60328604fd9ba0278ba04
Mitsikeli1 <silot012@gmail.com>
2016-04-16 16:43:21 +0300

    shareActionProvider

--- TrueColor/src/com/MrRobot/truecolor/MainActivity.java
+++ TrueColor/src/com/MrRobot/truecolor/MainActivity.java
@@ -1,11 +1,16 @@
 package com.MrRobot.truecolor;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +22,8 @@ public class MainActivity extends Activity {
 	TextView showColorName;
 	TextView showColor;
 	ImageView image;
+	MenuItem item;
+	ShareActionProvider shareActionProvider;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +31,39 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
    
     
-        //capture = (ImageButton)
-    
-    
-    
+        capture = (ImageButton)findViewById(R.id.imageButton1);
+        save = (ImageButton)findViewById(R.id.SaveButton);
+        showColorName = (TextView)findViewById(R.id.showColorName);
+        showColor = (TextView)findViewById(R.id.textView1);
+        image = (ImageView)findViewById(R.id.imageView1);
+        
+        
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/font.otf");
+        showColorName.setTypeface(font);
+        
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) {
+		//Share Button
+		getMenuInflater().inflate(R.menu.main, menu);
+		item = menu.findItem(R.id.Share);
+		shareActionProvider = (ShareActionProvider)item.getActionProvider();
+        setIntent("Hello");
         
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
         
-    }
+		return  true;
+		
+	}
+	
+	
+	private void setIntent(String text){
+		//Post from share button
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_TEXT, text);
+		shareActionProvider.setShareIntent(intent);
+	}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -45,15 +72,45 @@ public class MainActivity extends Activity {
     	
     	case R.id.About:
     		Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
+    		String message  = "App name: True Color \nCreated By: Mr.Robot \nHackathon Serres 2016 TEICM ";
+			AlertDialog.Builder ABuilder =  new AlertDialog.Builder(MainActivity.this);
+			ABuilder.setMessage(message)
+					.setCancelable(false)
+				    .setPositiveButton("OK",new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							
+							dialog.cancel();
+							
+							
+						}
+					});
+			AlertDialog alert = ABuilder.create();
+			alert.setTitle("About");
+			alert.show();
     		break;
     	case R.id.Help:
     		Toast.makeText(getApplicationContext(), "Help", Toast.LENGTH_SHORT).show();
+    		String helpMessage= "option 1: Capture image and tap the screen to get the color" +
+    				"\noption 2: load picture from file" +
+    				"\noption 3: Share the app";
+    		AlertDialog.Builder BBuilder =  new AlertDialog.Builder(MainActivity.this);
+			BBuilder.setMessage(helpMessage)
+					.setCancelable(false)
+				    .setPositiveButton("OK",new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							
+							dialog.cancel();
+							
+							
+						}
+					});
     		break;
     	case R.id.Save:
     		Toast.makeText(getApplicationContext(), "Save", Toast.LENGTH_SHORT).show();
-    		break;
-    	case R.id.Share:
-    		Toast.makeText(getApplicationContext(), "Share Everywhere", Toast.LENGTH_SHORT).show();
     		break;
     		
     	}
